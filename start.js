066615d7b68780b6ef0d802b1257085ab145bb36
calem48 <messaoud.messaoud92@gmail.com>
2023-04-15 18:51:19 +0100

    test

--- start.js
+++ start.js
@@ -1,39 +1,40 @@
-const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true, // add this line
-      devTools: false, // add this line
-    },
-  });
-
-  mainWindow.setMenuBarVisibility(false); // add this line
-  mainWindow.loadFile("index.html");
-
-  mainWindow.webContents.on("did-finish-load", function () {
-    const contentSize = mainWindow.getContentSize();
-    console.log(contentSize);
-    const width = contentSize[0] + 20;
-    const height = contentSize[1];
-    mainWindow.setSize(width, height);
-  });
-}
-app.on("ready", function () {
+    mainWindow = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true, // add this line
+        devTools: false // add this line
+      }
+    });
+  
+    mainWindow.setMenuBarVisibility(false); // add this line
+    mainWindow.loadFile('index.html');
+    
+    mainWindow.webContents.on('did-finish-load', function() {
+      const contentSize = mainWindow.getContentSize();
+      console.log(contentSize)
+      const width = contentSize[0]+20;
+      const height = contentSize[1];
+      mainWindow.setSize(width, height);
+    });
+  }
+app.on('ready', function () {
   createWindow();
 });
 
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", function () {
+app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
