b2bd9340fa26cabde3f3fe3006f1bec07f9e0313
abdomarrok <marrokmar@gmail.com>
2023-04-15 05:40:41 +0100

    first commit

---
+++ start.js
@@ -0,0 +1,43 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true, // add this line
+        devTools: false // add this line
+      }
+    });
+  
+    mainWindow.setMenuBarVisibility(false); // add this line
+    mainWindow.loadFile('index.html');
+    
+    mainWindow.webContents.on('did-finish-load', function() {
+      const contentSize = mainWindow.getContentSize();
+      console.log(contentSize)
+      const width = contentSize[0]+20;
+      const height = contentSize[1];
+      mainWindow.setSize(width, height);
+    });
+  }
+app.on('ready', function () {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// Code to set up your VPN interface using your VPN library goes here.
