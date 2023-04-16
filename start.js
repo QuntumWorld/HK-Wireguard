7137b79cae7fa02fd66deac12a7413016ef64248
calem48 <messaoud.messaoud92@gmail.com>
2023-04-16 01:50:06 +0100

    test

--- start.js
+++ start.js
@@ -2,19 +2,19 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 
 app.whenReady().then(() => {
   const window = new BrowserWindow({
-    // resizable: false,
+    resizable: false,
     frame: false,
     autoHideMenuBar: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       // enableRemoteModule: true, // add this line
-      devTools: true, // add this line
+      // devTools: true, // add this line for open dev console
     },
   });
 
   window.loadFile("index.html");
-  window.openDevTools();
+  // window.openDevTools();
 
   ipcMain.on("minimize", function (event, data) {
     window.minimize();
