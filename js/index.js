89c5a286f9cbb7f5583257f3c8479342b79327f7
abdomarrok <marrokmar@gmail.com>
2023-04-24 19:08:08 +0100

    add server selections

--- js/index.js
+++ js/index.js
@@ -1,11 +1,88 @@
 const { ipcRenderer } = require("electron");
 
+const servers = require('./js/servers');
 const { connect, disconnect } = require("./js/HK_Client/client");
 
 const mini = document.querySelector(".btns .mini");
 const closeWin = document.querySelector(".btns .close img");
 
 const onOffImg = document.querySelector(".on_off img");
+
+
+
+
+// fill the table with severs name and ip
+const tableBody = document.querySelector(".table_body");
+servers.servers.forEach((server) => {
+  const box = document.createElement("div");
+  box.className = "box";
+  const flag = document.createElement("i");
+  flag.className = "flag " + server.flag;
+  const countryName = document.createElement("span");
+  countryName.textContent = server.name;
+  const ip = document.createElement("p");
+  ip.textContent = server.ip;
+
+  box.appendChild(flag);
+  box.appendChild(countryName);
+  box.appendChild(ip);
+
+  tableBody.appendChild(box);
+});
+
+
+
+// Add an event listener to each box element in the table
+const boxElements = document.querySelectorAll(".box");
+boxElements.forEach((box, index) => {
+  // Get the server information for the current box
+  const server = servers.servers[index];
+  
+  // Add an event listener to the box
+  box.addEventListener("click", () => {
+    // Update the container element with the corresponding server information
+    const container = document.querySelector("div.body div.top div.container");
+    // Find the "container" element
+
+    // Clear any existing content
+    container.innerHTML = "";
+
+    // Create a new HTML string with the server information
+    const htmlString = `
+      <div class="title">
+        <h2>${server.name}</h2>
+        <h4>${server.name}</h4>
+      </div>
+      <div class="info">
+
+      <div class="protocol">
+      <h3>Wireguard</h3>
+      <h5>PROTOCOL</h5>
+    </div>
+        <div class="ip">
+          <h3>${server.ip}</h3>
+          <h5>IP</h5>
+        </div>
+        <div class="on_off">
+                <img src="img/off.svg" alt=""  id="on_off_button" />
+              </div>
+              <div class="upload">
+                <h3>6.4 MB/s</h3>
+                <h5>UPLOAD</h5>
+              </div>
+              <div class="download">
+                <h3>9.7 MB/s</h3>
+                <h5>DOWNLOAD</h5>
+              </div>
+            </div>
+      </div>
+    `;
+
+    // Set the HTML string as the content of the "container" element
+    container.innerHTML = htmlString;
+  });
+});
+
 
 // Define a function to toggle the state of the on/off button
 function toggleOnOff() {
@@ -39,4 +116,5 @@ mini.addEventListener("click", () => {
   // Send a message to the main process to minimize the window
   ipcRenderer.send("minimize");
 });
+
 
---
+++ js/servers.js
@@ -0,0 +1,25 @@
+
+const servers = [
+  {
+    name: "London",
+    flag: "flag-england",
+    ip: "206.189.18.1",
+  },
+  {
+    name: "Canada",
+    flag: "flag-canada",
+    ip: "120.22.23.1",
+  },
+  {
+    name: "France",
+    flag: "flag-france",
+    ip: "120.22.23.1",
+  },
+  {
+    name: "Germany",
+    flag: "flag-germany",
+    ip: "120.22.23.1",
+  },
+];
+exports.servers = servers;
+
